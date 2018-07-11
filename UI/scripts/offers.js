f4c3262fe2fee02206f86e3492ff242b89dde1d3
Fasoro-Joseph Alexander <afasorojoseph@gmail.com>
2018-07-11 09:11:40 +0100

    Get all rides and save user on login and signup

---
+++ UI/scripts/offers.js
@@ -0,0 +1,72 @@
+const spinner = document.getElementById('spinner');
+const rideOffers = document.getElementById('rideOffers');
+
+const starting = document.getElementById('starting');
+const ending = document.getElementById('ending');
+
+const html = data => `<div class="offer">
+  <div class="offer-main-content">
+    <p>
+      <i class="fas fa-map-marker-alt fa-lg"></i>
+      <b>${data.destination.toUpperCase()}</b>
+    </p>
+    <p>
+      <i class="fas fa-calendar-alt fa-lg"></i>
+      <b>${data.departure_date.split('T')[0]}</b>
+    </p>
+    <p>
+      <i class="fas fa-clock fa-lg"></i>
+      <b>${data.departure_time}</b>
+    </p>
+    <p>
+      <i class="fas fa-arrow-left fa-lg"></i>
+      <b>${data.point_of_departure.toUpperCase()}</b>
+    </p>
+    <p>
+      <i class="fas fa-users fa-lg"></i>
+      <b>${data.vehicle_capacity}</b>
+    </p>
+  </div>
+  <a href="#" class="btn">
+    <i class="fas fa-plus"></i>Join Ride</a>
+</div>`;
+
+const token = localStorage.getItem('token');
+const fetchUrl = 'https://ride-my-way-app.herokuapp.com/api/v1/rides';
+const fetchUrlWithParams = `https://ride-my-way-app.herokuapp.com/api/v1/rides?destination=${ending}&startingPoint=${starting}`;
+const fetchData = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'x-access-token': token,
+  },
+};
+
+const getRideOffers = (url) => {
+  fetch(url, fetchData)
+    .then(response => response.json())
+    .then((data) => {
+      spinner.setAttribute('style', 'display: none');
+
+      if (data.status === 'success') {
+        if (data.data.length === 0) {
+          const div = document.createElement('div');
+          div.innerText = 'There are no deals at this time';
+
+          return rideOffers.appendChild(div);
+        }
+
+        data.data.forEach((datum) => {
+          rideOffers.innerHTML += html(datum);
+        });
+      }
+    });
+};
+
+if (!token) {
+  window.location.replace(`${window.location.protocol}//${window.location.host}/UI/index.html`);
+} else {
+  spinner.setAttribute('style', 'display: initial');
+
+  getRideOffers(fetchUrl);
+}
